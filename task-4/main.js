4baf7961890898907b2005e86888f8ab76c12c32
MacBook <macbook@MacBook-Air-MacBook.local>
2022-09-06 00:28:00 -0700

    exercise

--- task-4/main.js
+++ task-4/main.js
@@ -1,9 +1,17 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+prompt.get("input", function (err, result) {
+  let a = result.input.split(" ");
+  let max = parseInt(a[0]);
+  let min = parseInt(a[0]);
+  for (let i = 0; i < a.length; i++) {
+    if (parseInt(a[i]) > max) {
+      max = parseInt(a[i]);
+    }
+    if (parseInt(a[i]) < min) {
+      min = parseInt(a[i]);
+    }
+  }
+  console.log(max - min);
 });
