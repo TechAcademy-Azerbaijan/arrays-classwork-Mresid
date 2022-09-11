f774afbe3bc27565879f720d35e0bea17e608cab
MacBook <macbook@Air-MacBook.bbrouter>
2022-09-11 02:14:52 -0700

    exercise

--- task-6/main.js
+++ task-6/main.js
@@ -4,6 +4,17 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+   let a = result.input.split(" ");
+   let max = parseInt(a[0]);
+   let min = parseInt(a[0]);
+  for (let i = 0; i < a.length; i++) {
+   if (parseInt(a[i]) > max) {
+    max = (a[i])
+   }if (parseInt(a[i])< min ) {
+    min = (a[i]);
+   }
+    
+  }
+  console.log(min);
+  console.log(max);
 });
